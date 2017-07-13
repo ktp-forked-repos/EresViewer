a97a7b367852ae9ba1df8f72632bb2f228452d84
adrianpacholec <adrianpacholec@gmail.com>
2017-07-13 16:31:15 +0200

    Jest ok
    Jakoś to wygląda, jeszcze będzie na bank zmieniane

--- eresViewer/app/src/main/java/com/example/maciek/eresviewer/ExpandableListAdapter.java
+++ eresViewer/app/src/main/java/com/example/maciek/eresviewer/ExpandableListAdapter.java
@@ -1,16 +1,11 @@
 package com.example.maciek.eresviewer;
 
 import android.content.Context;
-import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -21,12 +16,11 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
     private Context context;
     private List<Mark> listDataHeader;
-    private HashMap<String,List<String>> listHashMap;
 
-    public ExpandableListAdapter(Context context, List<Mark> listDataHeader, HashMap<String, List<String>> listHashMap) {
+
+    public ExpandableListAdapter(Context context, List<Mark> listDataHeader) {
         this.context = context;
         this.listDataHeader = listDataHeader;
-        this.listHashMap = listHashMap;
     }
 
     @Override
@@ -35,10 +29,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     }
 
     @Override
-    public int getChildrenCount(int groupPosition) {
-       // return listHashMap.get(listDataHeader.get(groupPosition).getMarkTitle()).size();
-        return 1;
-    }
+    public int getChildrenCount(int groupPosition) { return 1; }
 
     @Override
     public Object getGroup(int groupPosition) {
@@ -46,10 +37,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     }
 
     @Override
-    public Object getChild(int groupPosition, int childPosition) {
-        //return listHashMap.get(listDataHeader.get(groupPosition).getMarkTitle()).get(childPosition);
-        return null;
-    }
+    public Object getChild(int groupPosition, int childPosition) { return null; }
 
     @Override
     public long getGroupId(int groupPosition) {
@@ -57,9 +45,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     }
 
     @Override
-    public long getChildId(int groupPosition, int childPosition) {
-        return childPosition;
-    }
+    public long getChildId(int groupPosition, int childPosition) {return childPosition; }
 
     @Override
     public boolean hasStableIds() {
@@ -68,17 +54,13 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getGroupView(int position, boolean isExpanded, View convertView, ViewGroup parent) {
-
-        Mark currentMark = (Mark)getGroup(position);
-
-        if (convertView == null)
-        {
-            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.list_group,null);
-
+        Mark currentMark = (Mark) getGroup(position);
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_group, null);
         }
-        TextView lblListHeader = (TextView) convertView.findViewById(R.id.markName);
-        lblListHeader.setText(currentMark.getMarkTitle());
+        TextView markName = (TextView) convertView.findViewById(R.id.markName);
+        markName.setText(currentMark.getMarkTitle());
 
         TextView myMarkTextView = (TextView) convertView.findViewById(R.id.myMark);
         myMarkTextView.setText(Float.toString(currentMark.getMyMark()));
@@ -88,12 +70,10 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        //final String childText = (String)getChild(groupPosition,childPosition);
-        Mark currentMark = (Mark)getGroup(groupPosition);
-        if (convertView==null)
-        {
-            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.list_item,null);
+        Mark currentMark = (Mark) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_item, null);
         }
 
         TextView lowerMarkTextView = (TextView) convertView.findViewById(R.id.lowerMark);
@@ -108,12 +88,11 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         TextView amountOfMarksTextView = (TextView) convertView.findViewById(R.id.amountOfMarks);
         amountOfMarksTextView.setText(Float.toString(currentMark.getAmountOfMarks()));
 
-
         return convertView;
     }
 
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
-        return true;
+        return false;
     }
 }
--- eresViewer/app/src/main/java/com/example/maciek/eresviewer/SubjectActivity.java
+++ eresViewer/app/src/main/java/com/example/maciek/eresviewer/SubjectActivity.java
@@ -3,9 +3,7 @@ package com.example.maciek.eresviewer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ExpandableListView;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -17,77 +15,43 @@ public class SubjectActivity extends AppCompatActivity {
     private ExpandableListView listView;
     private ExpandableListAdapter markAdapter;
     private List<Mark> marks;
-    private HashMap<String, List<String>> listHash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-/*
-        marks=new ArrayList<>();
-        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
-        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
-        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
-        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
-        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
-        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
-        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
-        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
-        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
-        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
-        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
-        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
-        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
-        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
-        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
-        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
-        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
-        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
-        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
-        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
-        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
-        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
-        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
-        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
-
-*/
         initData();
-
         setContentView(R.layout.activity_subject);
-
-        // MarkAdapter markAdapter=new MarkAdapter(this,marks);
-        markAdapter = new ExpandableListAdapter(this, marks, listHash);
-        // ListView listView=(ListView) findViewById(R.id.listview_mark);
+        markAdapter = new ExpandableListAdapter(this, marks);
         listView = (ExpandableListView) findViewById(R.id.listview_mark);
-        // listView.setAdapter(markAdapter);
         listView.setAdapter(markAdapter);
 
     }
 
     private void initData() {
         marks = new ArrayList<>();
-        listHash = new HashMap<>();
-
-        marks.add(new Mark("Kolokwium 1", 5,5,5,5,50));
-        marks.add(new Mark("Kolokwium 2", 5,5,5,5,51));
-        marks.add(new Mark("Kolokwium 3", 5,5,5,5,52));
-
-        List<String> jeden = new ArrayList<>();
-        jeden.add("This is expandable listView");
-
-        List<String> dwa = new ArrayList<>();
-        dwa.add("Rozszerzamy");
-        dwa.add("ile");
-        dwa.add("chcemy");
-
-        List<String> trzy = new ArrayList<>();
-        trzy.add("Rozszerzamy");
-        trzy.add("ile");
-        trzy.add("chcemy");
-
-        listHash.put(marks.get(0).getMarkTitle(), jeden);
-        listHash.put(marks.get(1).getMarkTitle(), dwa);
-        listHash.put(marks.get(2).getMarkTitle(), trzy);
-
+        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
+        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
+        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
+        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
+        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
+        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
+        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
+        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
+        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
+        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
+        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
+        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
+        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
+        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
+        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
+        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
+        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
+        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
+        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
+        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
+        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
+        marks.add(new Mark("Kolokwium1", 5,5,5,5,50));
+        marks.add(new Mark("Kolokwium2", 5,5,5,5,51));
+        marks.add(new Mark("Kolokwium3", 5,5,5,5,52));
     }
 }
